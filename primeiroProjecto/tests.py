d7a84f0e57148992d0c2f9afa387064ed6e48983
Munzambi-Miguel <35455479+Munzambi-Miguel@users.noreply.github.com>
2024-02-01 00:57:30 +0100

    etapa dos testes vencido

--- primeiroProjecto/tests.py
+++ primeiroProjecto/tests.py
@@ -23,24 +23,11 @@ class MinhaAppsSegundoTests(TestCase):
         # Verifique se a resposta tem o status 200 (OK)
         self.assertEqual(response.status_code, 200)
         
-        ano_atual = int(datetime.datetime.now().strftime('%Y'))
-        
+        ano_atual = int(datetime.datetime.now().strftime('%Y'))        
                 
-        documento = "<h1> O ano inserigo %s" %(ano_atual - 1992)
-        
-        
+        documento = "<h1> O ano inserigo %s" %(ano_atual - 1992)                
         
         self.assertContains(response, documento)
     
 
-#     def test_segunda_tentativa(self):
-#         # Crie um cliente de teste
-#         response = self.client.get(reverse('nome_da_sua_url', args=[2022]))
 
-#         # Verifique se a resposta tem o status 200 (OK)
-#         self.assertEqual(response.status_code, 200)
-
-#         # Verifique se o conteúdo da resposta é o esperado
-#         ano_atual = datetime.datetime.now().strftime('%Y')
-#         esperado = f"<h1> O ano inserido {int(ano_atual) - 2022}</h1>"
-#         self.assertContains(response, esperado)
